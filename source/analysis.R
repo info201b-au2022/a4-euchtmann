8d0ebcddefbf609291277d850a377c0da835ee2c
euchtmann <ellauchtmann@gmail.com>
2022-11-21 18:51:47 -0800

    scatter plot and map, considering updating map in something more general

--- source/analysis.R
+++ source/analysis.R
@@ -21,15 +21,12 @@ test_query2 <- function(num=6) {
 
 ## Section 2  ---- 
 #----------------------------------------------------------------------------#
-# Your functions and variables might go here ... <todo: update comment>
-library("dplyr")
+# Data Summary
 
-# call function get_data to view data
+# data wrangling for race populations in prison
 data <- get_data(num_records = -1)
-View(data)
+# View(data)
 
-# drop NA values as well as select relevant columns 
-# to values of interest
 prison_data <- data %>%
   drop_na() %>%
   select(year, 
@@ -42,9 +39,9 @@ prison_data <- data %>%
          white_prison_pop)
 View(prison_data)
 
-#value 1 function 
+# Value 1 function 
 # Which race has the highest cumulative prison population count?
-high_prison_count <- data %>%
+high_prison_count <- prison_data %>%
   summarize(
     aapi_prison_pop = sum(aapi_prison_pop), 
     black_prison_pop = sum(black_prison_pop), 
@@ -58,10 +55,11 @@ high_prison_count <- data %>%
   filter(total_race_pop == max(total_race_pop)) %>%
   pull(race_prison_pop)
 high_prison_count
+#black_prison_pop
 
-#value 2 function
+# Value 2 function
 # Which race has the lowest cumulative prison population count?
-low_prison_count <- data %>%
+low_prison_count <- prison_data %>%
   summarize(
     aapi_prison_pop = sum(aapi_prison_pop), 
     black_prison_pop = sum(black_prison_pop), 
@@ -75,10 +73,11 @@ low_prison_count <- data %>%
   filter(total_race_pop == min(total_race_pop)) %>%
   pull(race_prison_pop)
 low_prison_count
+#aapi_prison_pop
 
-#value 3 function
+# Value 3 function
 # What race currently has the highest prison count?
-current_high_prison_count <- data %>%
+current_high_prison_count <- prison_data %>%
   filter(year == max(year)) %>%
   summarize(
     aapi_prison_pop = sum(aapi_prison_pop), 
@@ -93,10 +92,11 @@ current_high_prison_count <- data %>%
   filter(total_race_pop == max(total_race_pop)) %>%
   pull(race_prison_pop)
 current_high_prison_count
+#black_prison_pop
 
-# value 4
+# Value 4
 # What race currently has the lowest prison count?
-current_low_prison_count <- data %>%
+current_low_prison_count <- prison_data %>%
   filter(year == max(year)) %>%
   summarize(
     aapi_prison_pop = sum(aapi_prison_pop), 
@@ -111,22 +111,14 @@ current_low_prison_count <- data %>%
   filter(total_race_pop == min(total_race_pop)) %>%
   pull(race_prison_pop)
 current_low_prison_count
+#aapi_prison_pop
 
-# 
 #----------------------------------------------------------------------------#
-
 ## Section 3  ---- 
 #----------------------------------------------------------------------------#
 # Growth of the U.S. Prison Population
-# Your functions might go here ... <todo:  update comment>
-#----------------------------------------------------------------------------#
-# This function ... <todo:  update comment>
-# Use DPLYR and ggplot2 to replicate Figure 1 (above).  That is, produce a bar 
-# chart that shows the growth of the U.S. prison population from 1970 to 2018.  
-# To organize your code, you should create two functions: 
 
-# This data wrangling function should return a data frame that is suitable for 
-# visualization. This function takes no parameters. 
+# This function wrangles data to show to increase in jail populations from 1970 to 2018
 get_year_jail_pop <- function() {
   bar_chart_data <- data %>% 
     arrange(year) %>%
@@ -136,15 +128,14 @@ get_year_jail_pop <- function() {
     drop_na() %>%
     group_by(year) %>%
     summarize(total_jail_pop = sum(total_jail_pop))
-    # prettyNum(total_jail_pop, big.mark = ",", scientific = FALSE)
-  return(bar_chart_data)   
+  return(bar_chart_data)
 }
 
 
-
-# This function ... <todo:  update comment>
-# This plotting function should return the chart. This function: (1) Takes no 
-# parameters; and (2) Should call the data wrangling function.
+# This function creates the bar chart of jail populations from 1970 to 2018
+# (1) Takes no parameters
+# (2) Should call the data wrangling function.
+# DO NOT KNOW WHERE TO CALL DATA WRANGLING FUNCTION
 plot_jail_pop_for_us <- function()  {
   plot <- ggplot(data = bar_chart_data) +
     geom_col(
@@ -157,25 +148,28 @@ plot_jail_pop_for_us <- function()  {
       x = "Year", 
       y = "Total Jail Population", 
       title = "Increase of Jail Population in U.S. (1970 - 2018)"
-    ) 
+    ) +
   #format(total_jail_pop, big.mark = ",", scientific = FALSE)
-  #options(scipen = 999)
+  options(scipen = 999) # only way I've found that works, but still no commas
   return(plot)   
 } 
 
+# odd error but chart still works - 
+  # Error in `ggplot_add()`: 
+  # Can't add `o` to a ggplot object
 chart <- plot_jail_pop_for_us()
 chart
+
+
+
+#----------------------------------------------------------------------------#
 ## Section 4  ---- 
 #----------------------------------------------------------------------------#
 # Growth of Prison Population by State 
-# Your functions might go here ... <todo:  update comment>
-# See Canvas
-# Use DPLYR and ggplot2 to produce a line chart that shows the growth of the U.S. 
-# prison population from 1970 to 2018 by one or more states. 
-# You should write two functions: 
 
-# 1. This data wrangling function should return a data frame that is suitable 
-# for visualization. The parameter states should be a vector of states
+# This function 
+# parameter states : vector of states
+states = c("WA", "OR", "CA")
 
 get_jail_pop_by_states <- function(states) {
   line_chart_data <- data %>%
@@ -187,19 +181,19 @@ get_jail_pop_by_states <- function(states) {
     summarize(total_jail_pop = sum(total_jail_pop))
   return(line_chart_data)
 }
-
 test <- get_jail_pop_by_states(c("WA", "OR", "CA"))
 test
 
-# 2. This plotting function should return the chart. The parameter states should
-# be a vector of states. This function should call the data wrangling function.
+# This function creates a line chart of prison population growth for multiple states from 1970 to 2018
+# 1.) parameter states : vector of states
+# 2.) This function should call the data wrangling function.
+# DO NOT KNOW WHERE TO CALL DATA WRANGLING FUNCTION
 plot_jail_pop_by_states <- function(states) {
   plot <- ggplot(data = line_chart_data) +
     geom_line(
       mapping = aes(
         x = year, 
         y = total_jail_pop, 
-        group = states, 
         color = states, 
       )
     ) + 
@@ -215,79 +209,88 @@ test <- plot_jail_pop_by_states(c("WA", "OR", "CA"))
 test
 
 #----------------------------------------------------------------------------#
-
 ## Section 5  ---- 
 #----------------------------------------------------------------------------#
-# <variable comparison that reveals potential patterns of inequality>
-# Your functions might go here ... <todo:  update comment>
-# See Canvas
-# two different continuous varibable representing a trend in the dataset
-# Scatter plot
-# black men in prison versus men prison population?
+# Black v. White Prison Population Trend 
 
-scatter_plot_data <- data %>%
-  drop_na() %>%
-  group_by(year) %>%
-  select(year, male_prison_pop, black_prison_pop) %>%
-  summarize(male_prison_pop = sum(male_prison_pop), 
-            black_prison_pop = sum(black_prison_pop)
-            )
-View(scatter_plot_data)
+# This function wrangles data comparing the prison populations of Black and White people
+# from 2000 to 2013
+scatter_data_wrangling <- function(data) {
+  scatter_plot_data <- data %>%
+    drop_na() %>%
+    group_by(year) %>%
+    select(year, black_prison_pop, white_prison_pop) %>%
+    summarize(black_prison_pop = sum(black_prison_pop), 
+              white_prison_pop = sum(white_prison_pop)
+    ) %>%
+    pivot_longer("black_prison_pop":"white_prison_pop", 
+                 names_to = "race", 
+                 values_to = "population")
+  return(scatter_plot_data)
+  }
 
-plot <- ggplot(data = scatter_plot_data) +
-  geom_point(
-    mapping = aes(
-      x = male_prison_pop, 
-      y = black_prision_pop
-    )
-  ) + labs(
-    x = "Male Prison Population", 
-    y = "Black Prison Population", 
-    title = "Male Prison Population versus Black Prison Population (2000 - 2013)"
-      )
-
+# This function creates a 
 scatter_plot <- function() {
   plot <- ggplot(data = scatter_plot_data) +
     geom_point(
       mapping = aes(
-        x = male_prison_pop, 
-        y = black_prison_pop 
-        #group = states, 
-        #color = states, 
-      )
-    ) + 
+        x = year, 
+        y = population, 
+        color = race )
+      ) + 
     labs(
-      x = "Male Prison Population", 
-      y = "Black Prison Population", 
-      title = "Male Prison Population versus Black Prison Population (2000 - 2013)"
+      x = "Year", 
+      y = "Prison Population", 
+      title = "Black Prison Population versus White Prison Population (2000 - 2013)"
     )
   return(plot)
 }
 
-
-
-scatter_data_wrangling <- function(data) {
-}
-
-scatter_plot <- function() {
-  
-}
-
-
+plot <- scatter_plot()
+plot
 
 #----------------------------------------------------------------------------#
 
 ## Section 6  ---- 
 #----------------------------------------------------------------------------#
-# <a map shows potential patterns of inequality that vary geographically>
-# Your functions might go here ... <todo:  update comment>
-# See Canvas
-
 # one coninuous varibale by state
-# Mapping ration of black_prison_pop total to total_prison_pop
-# by current year
+# Mapping ration of black_prison_pop total to white_prison_pop
+# by current year 
 
 
+# This function wrangles data to show the current ratio of black to white people 
+# in the prison population.  
+
+map_data <- data %>%
+  drop_na() %>%
+  filter(year == max(year)) %>%
+  group_by(state) %>%
+  summarize(
+    black_prison_pop = sum(black_prison_pop), 
+    white_prison_pop = sum(white_prison_pop),
+    ratio = black_prison_pop / white_prison_pop
+  ) %>%
+  select(state, ratio)
+View(map_data)
+
+# This function creates a map that shows the current ratio of black to white people
+# in the prison population
+library(usmap)
+
+p <- plot_usmap(
+  regions = c("state"), 
+  data = map_data, 
+  values = "ratio", 
+  label = TRUE, 
+  label_color = "black"
+) +
+  scale_fill_continuous(
+    low = "white", 
+    high = "red", 
+    name = "Black to White Ratio"
+  ) +
+  labs(title = "Black to White Prison Population in Each State")
+p
 
 
 
